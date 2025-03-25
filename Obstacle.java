50c206449e39143ea20bc0202b3eaa43d30b4ed6
Anish Kommireddy <562601@my.cuhsd.org>
2025-03-25 14:49:44 -0700

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -1,1 +1,25 @@
-
+public class Obstacle {
+    private int x;
+    private int y;
+    private int speed;
+    
+    public Obstacle(int x, int y, int speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+    public void move(){
+        x-=speed;
+        if(x<-1){
+            x=18*5+x;
+            y=6+((int)(Math.random()*(4)))*3;
+        }
+        
+    }
+    public int getY(){
+        return y;
+    }
+    public int getX(){
+        return x;
+    }
+}
