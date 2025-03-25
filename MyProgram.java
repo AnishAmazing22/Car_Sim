7bf6242e39c2807f619cd999a14edcb9bb94e3e1
Anish Kommireddy <562601@my.cuhsd.org>
2025-03-25 14:49:27 -0700

    Update MyProgram.java

--- MyProgram.java
+++ MyProgram.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class MyProgram
+{
+    public static Game game;
+    
+    public static void main(String[] args)
+    {
+        Car car = new Ferrari();
+        game = new Game(car);
+        game.printScreen();
+        Scanner in  = new Scanner(System.in);
+        
+        while(true){
+            game.step(in.nextLine());
+        }
+    }
+}
