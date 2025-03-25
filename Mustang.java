7bcaed822abb6fab5fcadbfe2641e5f57b6cf979
Anish Kommireddy <562601@my.cuhsd.org>
2025-03-25 14:49:16 -0700

    Update Mustang.java

--- Mustang.java
+++ Mustang.java
@@ -1,1 +1,12 @@
-
+public class Mustang extends Car {
+    public Mustang(){
+        super(1.1, 1.5, 5, "\u001B[34m\u001B[44m");
+    }
+    public void powerUp(){
+        super.move(1);
+        super.move(1);
+        super.move(1);
+        super.move(1);
+        
+    }
+}
