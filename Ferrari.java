f3aea30153959d95e8e79d5990c945298ec8ffd6
Anish Kommireddy <562601@my.cuhsd.org>
2025-03-25 14:48:41 -0700

    Update Ferrari.java

--- Ferrari.java
+++ Ferrari.java
@@ -1,1 +1,12 @@
-
+public class Ferrari extends Car {
+    public Ferrari(){
+        super(1.1, 1.5, 5, "\u001B[35m\u001B[45m");
+    }
+    public void powerUp(){
+        super.move(-1);
+        super.move(-1);
+        super.move(-1);
+        super.move(-1);
+        
+    }
+}
