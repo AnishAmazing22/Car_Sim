43721fce4a1aa5fe3ce356c00ba930a04e38b308
Anish Kommireddy <562601@my.cuhsd.org>
2025-03-25 14:49:59 -0700

    Update Porsche.java

--- Porsche.java
+++ Porsche.java
@@ -1,1 +1,17 @@
+public class Porsche extends Car {
+    public Porsche(){
+        super(0.9, 1.9, 3, "\u001B[33m\u001B[43m");
+    }
+    public void powerUp(){
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        MyProgram.game.moveBack();
+        
 
+        
+    }
+}
